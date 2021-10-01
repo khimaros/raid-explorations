1a6d38b5c41a9b61d152db21ead6c6047b7183a4
khimaros <me@khimaros.com>
2021-10-01 15:51:08 -0700

    put efi on raid volume

--- prepare.sh
+++ prepare.sh
@@ -5,6 +5,7 @@ set -ex
 . ./config.sh
 . ./${RAID_EXPLORATION}/common.sh
 
+
 sed -i 's/ main/ main contrib/g' /etc/apt/sources.list
 
 [[ -z "${DEBIAN_BACKPORTS}" ]] || cat <<EOF > /etc/apt/sources.list.d/backports.list
@@ -15,14 +16,18 @@ apt update && apt install -y "${EXTRA_PACKAGES[@]}"
 
 apt install -y gdisk dosfstools "${RAID_PACKAGES[@]}"
 
-#wipefs -a "${DISKS_DEVICES[@]}"
-#sleep 1
+for dev in /dev/md/*; do
+    mdadm --stop $dev
+done
+
+wipefs -a "${DISKS_DEVICES[@]}"
+sleep 1
 
 for disk in "${DISKS_DEVICES[@]}"; do
     sgdisk --zap-all $disk
 
     sgdisk -n1:1M:+512M -t1:EF00 $disk
-    sgdisk -n2:0:+512M -t2:8300 $disk
+    sgdisk -n2:0:+512M -t2:8301 $disk
     sgdisk -n3:0:0 -t3:8301 $disk
 
     #parted $disk mktable gpt
@@ -30,8 +35,12 @@ for disk in "${DISKS_DEVICES[@]}"; do
     #parted $disk set 1 esp on
     #parted $disk mkpart primary ext4 513MiB 1024MiB
     #parted $disk mkpart primary 1024MiB 100%
-
-    mkfs.msdos -F 32 -s 1 -n EFI ${disk}${DISKS_PART_PREFIX}1
 done
 
-apt install -y "${RAID_PACKAGES[@]}"
+EFI_DEVICES=($(eval echo "/dev/${DISKS_GLOB}${DISKS_PART_PREFIX}1"))
+
+apt install -y "${RAID_PACKAGES[@]}" mdadm
+
+mdadm --create /dev/md0 --metadata=1.0 --level=1 --raid-devices=4 --bitmap=internal "${EFI_DEVICES[@]}"
+
+mkfs.msdos -F 32 -s 1 -n EFI /dev/md0
