f0cf0a8884163c3eb783ad6d6ca0a9a27df7616b
khimaros <me@khimaros.com>
2021-10-01 07:53:11 -0700

    fix typo

--- dm-integrity~md~dm-crypt~lvm~ext4/common.sh
+++ dm-integrity~md~dm-crypt~lvm~ext4/common.sh
@@ -4,7 +4,7 @@ DISKS=($(eval echo "$DISKS_GLOB"))
 
 DISKS_DEVICES=($(eval echo "/dev/${DISKS_GLOB}"))
 
-BOOT_DEVICES=($(eval echo "/dev/mapper/${DISKS_GLOB}${DISKS_PART_PREFIX}2"))
+BOOT_DEVICES=($(eval echo "/dev/${DISKS_GLOB}${DISKS_PART_PREFIX}2"))
 
 ROOT_DEVICES=($(eval echo "/dev/mapper/${DISKS_GLOB}${DISKS_PART_PREFIX}3_int"))
 
