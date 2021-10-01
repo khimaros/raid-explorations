cc874ffe3fa987b92be56e1d4578d5fd03eaf16b
khimaros <me@khimaros.com>
2021-10-01 06:47:22 -0700

    try to install grub bins on all drives

--- bootloader.sh
+++ bootloader.sh
@@ -15,9 +15,11 @@ fi
 
 if [[ "$BOOT_MODE" = "efi" ]]; then
     umount /mnt/boot/efi
+		umount /mnt/boot
 
     for ((i=${#DISKS_DEVICES[@]}-1; i>=0; i--)); do
         disk="${DISKS_DEVICES[$i]}"
+        mount "${disk}${DISKS_PART_PREFIX}2" /mnt/boot/efi
         mount "${disk}${DISKS_PART_PREFIX}1" /mnt/boot/efi
         chroot /mnt grub-install --target=x86_64-efi \
             --bootloader-id=debian \
@@ -25,8 +27,10 @@ if [[ "$BOOT_MODE" = "efi" ]]; then
 				label="debian-${disk##/dev/}"
 				chroot /mnt efibootmgr -c -g -d ${disk} -p 1 -L "$label" -l '\EFI\debian\grubx64.efi'
 				umount /mnt/boot/efi
+				umount /mnt/boot
     done
 
+		mount "${DISKS_DEVICES[0]}${DISKS_PART_PREFIX}2" /mnt/boot
 		mount "${DISKS_DEVICES[0]}${DISKS_PART_PREFIX}1" /mnt/boot/efi
 else
     chroot /mnt dpkg-reconfigure grub-pc
