ee18039e1926941b066062f0673a59358215b918
khimaros <me@khimaros.com>
2021-10-02 14:29:32 -0700

    initial replace script for zfs

---
+++ dm-crypt~zfs/replace.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -ex
+
+. "$(dirname "$0")/../config.sh"
+. "$(dirname "$0")/common.sh"
+
+disks=("${BOOT_DEVICES[@]}")
+zpool list -vH bpool | awk '{ if ($10 == "FAULTED" || $10 == "UNAVAIL" || $10 == "OFFLINE") { print $1 } }' | while read uuid; do
+    disk=${disks[0]}
+    zpool replace bpool $uuid $disk
+    disks=("${disks[@]:1}")
+done
+
+disks=("${CRYPT_DEVICES[@]}")
+zpool list -vH rpool | awk '{ if ($10 == "FAULTED" || $10 == "UNAVAIL" || $10 == "OFFLINE") { print $1 } }' | while read uuid; do
+    disk=${disks[0]}
+    zpool replace rpool $uuid $disk
+    disks=("${disks[@]:1}")
+done
