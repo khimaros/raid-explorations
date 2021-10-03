95a383a3a18030a18d5b34f173d31e68841e1cf8
khimaros <me@khimaros.com>
2021-10-02 17:34:21 -0700

    benchmark automation

--- tests/benchmark.sh
+++ tests/benchmark.sh
@@ -10,9 +10,15 @@ pushd benchmark
 
 sysbench fileio prepare
 
-sysbench fileio run --file-test-mode=rndwr --time=0 --events=5000
+for i in 1 2 3 4; do
+    echo "[*] rndwr benchmark #$i"
+    sysbench fileio run --file-test-mode=rndwr --time=0 --events=5000 | egrep '(total time|95th percentile):' | sed -r 's/[[:space:]]+/ /g'
+done
 
-sysbench fileio run --file-test-mode=seqwr --time=0 --events=20000
+for i in 1 2 3 4; do
+    echo "[*] seqwr benchmark #$i"
+    sysbench fileio run --file-test-mode=seqwr --time=0 --events=20000 | egrep '(total time|95th percentile):' | sed -r 's/[[:space:]]+/ /g'
+done
 
 popd
 
