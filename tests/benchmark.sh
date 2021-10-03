cce24af5d1de974d7765497110de1eaba0fc4c55
khimaros <me@khimaros.com>
2021-10-02 18:06:48 -0700

    better benchmark output

--- tests/benchmark.sh
+++ tests/benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 apt install -y sysbench
 
@@ -11,11 +11,13 @@ pushd benchmark
 sysbench fileio prepare
 
 for i in 1 2 3 4; do
+    echo
     echo "[*] rndwr benchmark #$i"
     sysbench fileio run --file-test-mode=rndwr --time=0 --events=5000 | egrep '(total time|95th percentile):' | sed -r 's/[[:space:]]+/ /g'
 done
 
 for i in 1 2 3 4; do
+    echo
     echo "[*] seqwr benchmark #$i"
     sysbench fileio run --file-test-mode=seqwr --time=0 --events=20000 | egrep '(total time|95th percentile):' | sed -r 's/[[:space:]]+/ /g'
 done
