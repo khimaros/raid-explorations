8a09f117fcb8a9efcbc71c75b33be68a0d4afd4c
khimaros <me@khimaros.com>
2021-10-02 18:25:46 -0700

    improve scrub

--- dm-integrity~md~dm-crypt~lvm~ext4/scrub.sh
+++ dm-integrity~md~dm-crypt~lvm~ext4/scrub.sh
@@ -2,6 +2,17 @@
 
 set -ex
 
+. "$(dirname "$0")/../config.sh"
+. "$(dirname "$0")/common.sh"
+
+mdadm --action=check /dev/${BOOT_MD}
+
+mdadm --wait /dev/${BOOT_MD}
+
+mdadm --detail /dev/${BOOT_MD}
+
 mdadm --action=check /dev/${ROOT_MD}
 
 mdadm --wait /dev/${ROOT_MD}
+
+mdadm --detail /dev/${ROOT_MD}
---
+++ scrub.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -ex
+
+. ./config.sh
+. ./${RAID_EXPLORATION}/common.sh
+
+mdadm --action=check /dev/md0
+
+mdadm --wait /dev/md0
+
+mdadm --detail /dev/md0
+
+./${RAID_EXPLORATION}/scrub.sh
