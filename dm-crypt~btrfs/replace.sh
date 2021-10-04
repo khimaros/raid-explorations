02c22823d3e5c3bc8162c7cefe84d4c7fac63019
khimaros <me@khimaros.com>
2021-10-04 08:13:04 -0700

    add btrfs replace

--- dm-crypt~btrfs/replace.sh
+++ dm-crypt~btrfs/replace.sh
@@ -5,4 +5,12 @@ set -ex
 . "$(dirname "$0")/../config.sh"
 . "$(dirname "$0")/common.sh"
 
+for dev in "${REPLACE_CRYPT_DEVICES[@]}"; do
+    btrfs device add $dev /boot
+		btrfs device remove missing /boot
+done
 
+for dev in "${REPLACE_CRYPT_DEVICES[@]}"; do
+    btrfs device add $dev /
+		btrfs device remove missing /
+done
