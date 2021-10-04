11a60ae05ff0575aa376aab175b0256cab7e4bf9
khimaros <me@khimaros.com>
2021-10-03 22:58:37 -0700

    force replace

--- dm-crypt~zfs/replace.sh
+++ dm-crypt~zfs/replace.sh
@@ -5,17 +5,19 @@ set -ex
 . "$(dirname "$0")/../config.sh"
 . "$(dirname "$0")/common.sh"
 
+# FIXME: iterate through the *_DEVICES lists.
+
 disks=("${BOOT_DEVICES[@]}")
 zpool list -vH bpool | awk '{ if ($10 == "FAULTED" || $10 == "UNAVAIL" || $10 == "OFFLINE") { print $1 } }' | while read uuid; do
     disk=${disks[0]}
-    zpool replace bpool $uuid $disk
+    zpool replace -f bpool $uuid $disk
     disks=("${disks[@]:1}")
 done
 
 disks=("${CRYPT_DEVICES[@]}")
 zpool list -vH rpool | awk '{ if ($10 == "FAULTED" || $10 == "UNAVAIL" || $10 == "OFFLINE") { print $1 } }' | while read uuid; do
     disk=${disks[0]}
-    zpool replace rpool $uuid $disk
+    zpool replace -f rpool $uuid $disk
     disks=("${disks[@]:1}")
 done
 
