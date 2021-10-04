fd21fcc7076963812223e2bca99f335edf344ed4
khimaros <me@khimaros.com>
2021-10-03 23:00:06 -0700

    close luks on remove

--- dm-crypt~zfs/remove.sh
+++ dm-crypt~zfs/remove.sh
@@ -5,7 +5,9 @@ set -ex
 . "$(dirname "$0")/../config.sh"
 . "$(dirname "$0")/common.sh"
 
-for disk in "${DISKS_DEVICES[@]}"; do
-    zpool offline bpool "${disk}2" || true
-    zpool offline rpool "${disk}3" || true
+for disk in "${DISKS[@]}"; do
+    dev="/dev/${dev}"
+    zpool offline bpool "${dev}2" || true
+    zpool offline rpool "${dev}3" || true
+		cryptsetup luksClose "${disk}3_crypt" || true
 done
