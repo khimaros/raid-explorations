ac4551922182c83cea00d990ca58b3d5e58fd432
khimaros <me@khimaros.com>
2021-10-04 12:10:38 -0700

    fix clone

--- dm-crypt~btrfs/clone.sh
+++ dm-crypt~btrfs/clone.sh
@@ -6,6 +6,7 @@ set -ex
 . "$(dirname "$0")/common.sh"
 
 for disk in "${REPLACE_DISKS[@]}"; do
+    dev="/dev/${disk}"
     crypttab_uuid=$(grep "${disk}${DISKS_PART_PREFIX}3_crypt" /etc/crypttab | awk '{ print $2 }' | cut -d= -f2)
     if [[ -n "$crypttab_uuid" ]]; then
         sgdisk -u3:$crypttab_uuid $dev
