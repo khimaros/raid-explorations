1cc2f06dedb67d3d2ed752355e2cb59c3dfcea8c
khimaros <me@khimaros.com>
2021-10-05 20:27:00 -0700

    cleanup output

--- dm-integrity~md~dm-crypt~lvm~ext4/common.sh
+++ dm-integrity~md~dm-crypt~lvm~ext4/common.sh
@@ -2,11 +2,7 @@ RAID_PACKAGES=(dosfstools cryptsetup mdadm lvm2)
 
 BOOT_DEVICES=($(eval echo "/dev/${DISKS_GLOB}${DISKS_PART_PREFIX}2"))
 
-REPLACE_BOOT_DEVICES=($(eval echo "/dev/${REPLACE_DISKS_GLOB}${DISKS_PART_PREFIX}2"))
-
 ROOT_DEVICES=($(eval echo "/dev/mapper/${DISKS_GLOB}${DISKS_PART_PREFIX}3_int"))
-
-REPLACE_ROOT_DEVICES=($(eval echo "/dev/mapper/${REPLACE_DISKS_GLOB}${DISKS_PART_PREFIX}3_int"))
 
 BOOT_MD_NAME=boot
 
@@ -19,3 +15,8 @@ ROOT_MD_DEVICE=/dev/md/${ROOT_MD_NAME}
 ROOT_CRYPT_NAME=md_${ROOT_MD_NAME}_crypt
 
 ROOT_CRYPT_DEVICE=/dev/mapper/${ROOT_CRYPT_NAME}
+
+if [[ -n "$REPLACE_DISKS_GLOB" ]]; then
+    REPLACE_BOOT_DEVICES=($(eval echo "/dev/${REPLACE_DISKS_GLOB}${DISKS_PART_PREFIX}2"))
+    REPLACE_ROOT_DEVICES=($(eval echo "/dev/mapper/${REPLACE_DISKS_GLOB}${DISKS_PART_PREFIX}3_int"))
+fi
